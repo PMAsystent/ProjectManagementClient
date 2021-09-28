b02ddadd65246674142749e8e9c2bd30ab51757d
Robert Jaskólski <rjaskolski@i2asolutions.com>
2021-09-28 20:21:31 +0200

    Added redux to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import createStore from './redux/store';
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/store.tsx
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import rootReducer from "./rootReducer";
+
+const createStore = () => {
+  return configureStore({
+    reducer: rootReducer
+  });
+};
+
+export default createStore;
