bcb7dfb5185bb95a5c990b7737b2f3ec400714cc
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-11-11 20:43:58 +0100

    Add dashboard layout component

---
+++ src/layouts/DashboardLayout/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useState } from 'react';
+import { Box } from '@mui/material';
+import './styles.scss';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
+
+const DashboardLayout: FC<any> = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <>
+      <div className="dashboard-layout-root">
+        <Box className="dashboard-layout-content">{children}</Box>
+      </div>
+      <Navbar onSidebarOpen={() => setSidebarOpen(true)} />
+      <Sidebar open={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
+    </>
+  );
+};
+
+export default DashboardLayout;
