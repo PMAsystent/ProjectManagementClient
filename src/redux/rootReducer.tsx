f19b9aa71e4993ab8f4c984934187d68efac1df8
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-11-22 16:52:01 +0100

    add project reducer to root reducer

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import { authReducer, authReducerInterface } from './auth/auth.slice';
+import { projectReducer } from './project/project.slice';
 
 export interface rootReducerInterface {
   auth: authReducerInterface;
+  projects: any;
 }
 
 const rootReducer = combineReducers<rootReducerInterface>({
   auth: authReducer.reducer,
+  projects: projectReducer.reducer,
 });
 
 export default rootReducer;
