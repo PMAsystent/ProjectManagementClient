5a44b62de5fe28ee7c7f30211507c21ea0455e93
Robert Jaskólski <rjaskolski@i2asolutions.com>
2021-12-01 20:27:12 +0100

    Fetching data added

--- src/containers/ProjectDetails/ProjectDetails.tsx
+++ src/containers/ProjectDetails/ProjectDetails.tsx
@@ -1,15 +1,26 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './styles.scss';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import BasicSpeedDial from '../../components/BasicSpeedDial/BasicSpeedDial';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { getDashboardPath } from '../../core/routes';
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import { format } from 'date-fns';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProject, selectProjectDetails, selectProjectDetailsFetchStatus } from '../../redux/project/project.slice';
+import { fetchStatues } from '../../core/enums/redux.statues';
+import { Avatar, Stack } from '@mui/material';
+import { stringToColor } from '../../core/utils';
 
 const ProjectDetails = () => {
+  const [activeTasks, setActiveTasks] = useState(0);
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const { projectid } = useParams<{ projectid: string }>();
+  const projectDetails = useSelector(selectProjectDetails);
+  const projectDetailsFetchStatus = useSelector(selectProjectDetailsFetchStatus);
+  const dispatch = useDispatch();
   const history = useHistory();
   const actions = useMemo(
     () => [
@@ -29,65 +40,89 @@ const ProjectDetails = () => {
         handleOnClick: () => {},
       },
     ],
-    []
+    [history]
   );
+
+  useEffect(() => {
+    dispatch(getProject(+projectid));
+  }, [dispatch, projectid]);
+
+  useEffect(() => {
+    if (projectDetailsFetchStatus === fetchStatues.FULFILLED) {
+      setActiveTasks(1);
+      setCompletedTasks(1);
+    }
+  }, [projectDetailsFetchStatus]);
 
   return (
     <section className="project-container">
-      <div className="project-header">
-        <div className="info">
-          <h1>Aplikacja Mobilna MBank</h1>
-          <div>
-            <p>Aktywne Zadania</p>
-            <h3>26</h3>
+      {projectDetailsFetchStatus === fetchStatues.FULFILLED && (
+        <>
+          <div className="project-header">
+            <div className="info">
+              <h1>{projectDetails?.name}</h1>
+              <div className="info-item">
+                <p>Aktywne Zadania</p>
+                <h3>{activeTasks}</h3>
+              </div>
+              <div className="info-item">
+                <p>Ukończone Zadania</p>
+                <h3>{completedTasks}</h3>
+              </div>
+              <div className="info-item">
+                <p>Typ Projektu</p>
+                <h3>Aplikacja</h3>
+              </div>
+              <div className="info-item">
+                <p>Data Rozpoczęcia</p>
+                <h3>{format(Date.now(), 'dd.MM.yyyy')}</h3>
+              </div>
+              <div className="info-item">
+                <p>Deadline</p>
+                <h3>{format(new Date(projectDetails?.dueDate || Date.now()), 'dd.MM.yyyy')}</h3>
+              </div>
+              <div className="info-item">
+                <p>Zespół</p>
+                <Stack direction="row" spacing={1}>
+                  {projectDetails?.projectAssignedUsers.map((user) => {
+                    const { userName } = user;
+                    return (
+                      <Avatar key={userName} sx={{ bgcolor: stringToColor(userName) }}>
+                        {userName[0].toUpperCase()}
+                      </Avatar>
+                    );
+                  })}
+                </Stack>
+              </div>
+            </div>
+            <div className="circle">
+              <CircularProgressbar
+                styles={buildStyles({
+                  backgroundColor: '#292929',
+                })}
+                background
+                backgroundPadding={projectDetails?.progressPercentage}
+                value={projectDetails?.progressPercentage || 0}
+                text={`${projectDetails?.progressPercentage}%`}
+              />
+            </div>
           </div>
-          <div>
-            <p>Ukończone Zadania</p>
-            <h3>45</h3>
+          <div className="project-tasks">
+            <div className="tasks-container">
+              <h1>Nierozpoczęte</h1>
+              <div className="tasks-drop"></div>
+            </div>
+            <div className="tasks-container">
+              <h1>W Trakcie</h1>
+              <div className="tasks-drop"></div>
+            </div>
+            <div className="tasks-container">
+              <h1>Ukończone</h1>
+              <div className="tasks-drop"></div>
+            </div>
           </div>
-          <div>
-            <p>Typ Projektu</p>
-            <h3>Aplikacja</h3>
-          </div>
-          <div>
-            <p>Data Rozpoczęcia</p>
-            <h3>{format(Date.now(), 'dd.MM.yyyy')}</h3>
-          </div>
-          <div>
-            <p>Deadline</p>
-            <h3>{format(Date.now(), 'dd.MM.yyyy')}</h3>
-          </div>
-          <div>
-            <p>Zespół</p>
-            <h3>icony</h3>
-          </div>
-        </div>
-        <div className="circle">
-          <CircularProgressbar
-            styles={buildStyles({
-              backgroundColor: '#292929',
-            })}
-            background
-            backgroundPadding={3}
-            value={75}
-            text={`${75}%`}
-          />
-        </div>
-      </div>
-      <div className="project-tasks">
-        <div className="tasks-container">
-          <h1>Nierozpoczęte</h1>
-          <div className="tasks-drop"></div>
-        </div>
-        <div className="tasks-container">
-          <h1>W Trakcie</h1>
-          <div className="tasks-drop"></div>
-        </div>
-        <div className="tasks-container">
-          <h1>Ukończone</h1>
-          <div className="tasks-drop"></div>
-        </div>
-      </div>
+        </>
+      )}
       <BasicSpeedDial actions={actions} />
     </section>
   );
--- src/redux/project/project.slice.tsx
+++ src/redux/project/project.slice.tsx
@@ -1,18 +1,22 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { instance } from '../../api';
 import { rootReducerInterface } from '../rootReducer';
-import { myProjectsType, postProjectType, projectType } from '../../core/types/api/project.requests.types';
+import { myProjectsType, postProjectType, projectDetails, projectType } from '../../core/types/api/project.requests.types';
 import SnackbarUtils from '../../core/utils/SnackbarUtils';
 
 export interface projectReducerInterface {
   projectList: Array<projectType>;
+  projectDetails: null | projectDetails;
   projectListFetchStatus: null | string;
+  projectDetailsFetchStatus: null | string;
   projectPostFetchStatus: null | string;
 }
 
 const INIT_STATE: projectReducerInterface = {
   projectList: [],
+  projectDetails: null,
   projectListFetchStatus: null,
+  projectDetailsFetchStatus: null,
   projectPostFetchStatus: null,
 };
 
@@ -26,6 +30,23 @@ export const getProjects = createAsyncThunk<any, void, { state: rootReducerInter
       .get<myProjectsType>('/MyProjects', { headers: { authorization: `Bearer ${accessToken}` } })
       .then((response) => {
         return response.data.projectsList ?? [];
+      })
+      .catch((error) => {
+        return rejectWithValue(error.response.data.title);
+      });
+  }
+);
+
+export const getProject = createAsyncThunk<any, number, { state: rootReducerInterface; rejectValue: string }>(
+  'project/getProject',
+  async (id, { rejectWithValue, getState }) => {
+    const {
+      auth: { accessToken },
+    } = getState();
+    return await instance
+      .get<myProjectsType>(`/MyProjects/${id}`, { headers: { authorization: `Bearer ${accessToken}` } })
+      .then((response) => {
+        return response.data;
       })
       .catch((error) => {
         return rejectWithValue(error.response.data.title);
@@ -61,6 +82,17 @@ export const projectReducer = createSlice({
   },
   extraReducers: (builder) => {
     builder
+      .addCase(getProject.pending, (state, action) => {
+        state.projectDetailsFetchStatus = action.meta.requestStatus;
+      })
+      .addCase(getProject.fulfilled, (state, action) => {
+        state.projectDetailsFetchStatus = action.meta.requestStatus;
+        state.projectDetails = action.payload;
+      })
+      .addCase(getProject.rejected, (state, action) => {
+        state.projectDetailsFetchStatus = action.meta.requestStatus;
+        SnackbarUtils.error('Pobranie projektu nie powiodło się');
+      })
       .addCase(getProjects.pending, (state, action) => {
         state.projectListFetchStatus = action.meta.requestStatus;
       })
@@ -70,6 +102,7 @@ export const projectReducer = createSlice({
       })
       .addCase(getProjects.rejected, (state, action) => {
         state.projectListFetchStatus = action.meta.requestStatus;
+        SnackbarUtils.error('Pobranie projektów nie powiodło się');
       })
       .addCase(postProject.pending, (state, action) => {
         state.projectPostFetchStatus = action.meta.requestStatus;
@@ -90,3 +123,5 @@ export const { clearProjectPostFetchStatus } = projectReducer.actions;
 export const selectProjects = (state: rootReducerInterface) => state.projects.projectList;
 export const selectLoadingProjects = (state: rootReducerInterface) => state.projects.projectListFetchStatus;
 export const selectProjectPostFetchStatus = (state: rootReducerInterface) => state.projects.projectPostFetchStatus;
+export const selectProjectDetails = (state: rootReducerInterface) => state.projects.projectDetails;
+export const selectProjectDetailsFetchStatus = (state: rootReducerInterface) => state.projects.projectDetailsFetchStatus;
