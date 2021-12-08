43d3fea59df14f382706868279139ae09b404875
Robert Jaskólski <rjaskolski@i2asolutions.com>
2021-12-08 18:50:47 +0100

    Add task priority

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,22 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './styles.scss';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { format } from 'date-fns';
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import { projectTask } from '../../core/types/api/task.request.types';
+import { taskPriority } from '../../core/enums/task.priority';
 
 const TaskItem: FC<{ task: projectTask }> = ({ task }) => {
+  const [priority, setPriority] = useState<{ orange: number[]; white: number[] }>({
+    orange: [],
+    white: [],
+  });
+
+  useEffect(() => {
+    if (task.priority === taskPriority.LOW)
+      setPriority({
+        orange: [1],
+        white: [2, 3, 4, 5],
+      });
+
+    if (task.priority === taskPriority.MEDIUM)
+      setPriority({
+        orange: [1, 2, 3],
+        white: [4, 5],
+      });
+
+    if (task.priority === taskPriority.HIGH)
+      setPriority({
+        orange: [1, 2, 3, 4, 5],
+        white: [],
+      });
+  }, [task]);
+
   return (
     <>
       <h2 className="task-title">{task.name}</h2>
       <div className="task-priority">
         <p>Priorytet</p>
         <span>
-          <FiberManualRecordIcon />
-          <FiberManualRecordIcon />
-          <FiberManualRecordIcon />
-          <FiberManualRecordIcon />
-          <FiberManualRecordIcon />
+          {priority.orange.map((n) => (
+            <FiberManualRecordIcon className="circle-orange" key={n} />
+          ))}
+          {priority.white.map((n) => (
+            <FiberManualRecordIcon key={n} />
+          ))}
         </span>
       </div>
       <div className="task-dueDate">
