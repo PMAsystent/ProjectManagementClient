43e4c31c4cb3a4c32a3c6a51ffa5e64d4187b29f
Sobiechh <p.sobieszczyk1@gmail.com>
2021-12-13 03:42:53 +0100

    codeClean + add step state

--- src/components/TreeProjectView/TreeProjectView.tsx
+++ src/components/TreeProjectView/TreeProjectView.tsx
@@ -7,6 +7,9 @@ import { useSelector } from 'react-redux';
 import { selectProjects } from '../../redux/project/project.slice';
 import './styles.scss';
 import { projectType } from 'core/types/api/project.requests.types';
+import AddStepModal from '../../containers/AddStepModal/AddStepModal';
+import { useHistory } from 'react-router-dom';
+import { projectStep } from '../../core/types/api/step.request.types';
 
 interface projectTypeWithClassName extends projectType {
   className: string;
@@ -22,11 +25,14 @@ const getInitialProjects = (projects: Array<projectType>): Array<projectTypeWith
 };
 
 const TreeProjectView: FC<any> = () => {
+  const history = useHistory();
   const projectsRedux = useSelector(selectProjects);
   const initialProjects: Array<projectTypeWithClassName> = getInitialProjects(projectsRedux);
 
   const [projects, setProjects] = useState<Array<projectTypeWithClassName>>(initialProjects);
-  const [expanded, setExpanded] = useState<Array<string>>([]);
+  const [expanded, setExpanded] = useState<Array<string>>(['1']);
+  const [addStepModal, setAddStepModal] = useState<boolean>(false);
+  const [projectId, setProjectId] = useState<number>(-1);
 
   const addSelectedClassName = (project: projectTypeWithClassName) => {
     const projectsCopy = initialProjects.map((project) => {
@@ -41,45 +47,69 @@ const TreeProjectView: FC<any> = () => {
 
     setProjects(projectsCopy);
   };
+
   const setExpandedList = (project: projectType) => {
     setExpanded([`${project.id}`]);
   };
 
+  const handleOnProjectClick = async (id: number) => {
+    history.push(`/project/${id}`);
+  };
+
+  const handleOnStepClick = (projectTd: number, stepId: number) => {
+    history.push(`/project/${projectTd}/${stepId}`);
+  };
+
   return (
-    <TreeView
-      aria-label="Apps navigator"
-      className="sidebar-tree"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      expanded={expanded}
-    >
-      {projects?.map((project) => {
-        return (
-          <TreeItem
-            key={project.id}
-            nodeId={`${project.id}`}
-            label={project.name}
-            className={project.className}
-            onClick={() => {
-              addSelectedClassName(project);
-              setExpandedList(project);
-            }}
-          >
-            {project.steps?.map((step: any) => {
-              return <TreeItem key={step.id} nodeId={`${project.id}_${step.id}`} label={`${step.name}`} className="tree-item__task" />;
-            })}
+    <>
+      <TreeView
+        aria-label="Apps navigator"
+        className="sidebar-tree"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        expanded={expanded}
+      >
+        {projects?.map((project) => {
+          return (
             <TreeItem
-              nodeId={`${project.id}_Add`}
-              label="Dodaj step"
-              className="tree-item__step-add"
+              key={project.id}
+              nodeId={`${project.id}`}
+              label={`${project.name}`}
+              className={project.className}
               onClick={() => {
-                // Action
+                setExpandedList(project);
+                addSelectedClassName(project);
+                handleOnProjectClick(project.id).then();
               }}
-            />
-          </TreeItem>
-        );
-      })}
-    </TreeView>
+            >
+              {project.steps?.map((step: projectStep) => {
+                return (
+                  <TreeItem
+                    key={step.id}
+                    nodeId={`${project.id}_${step.id}`}
+                    label={`${step.name} (${step.progressPercentage}%)`}
+                    className="tree-item__task"
+                    onClick={() => {
+                      handleOnStepClick(project.id, step.id);
+                    }}
+                  />
+                );
+              })}
+              <TreeItem
+                nodeId={`${project.id}_Add`}
+                label="Dodaj step"
+                className="tree-item__step-add"
+                onClick={() => {
+                  setProjectId(project.id);
+                  setAddStepModal(true);
+                }}
+              />
+            </TreeItem>
+          );
+        })}
+      </TreeView>
+      {addStepModal && <AddStepModal open={addStepModal} handleClose={() => setAddStepModal(false)} projectId={projectId} />}
+    </>
   );
 };
 
