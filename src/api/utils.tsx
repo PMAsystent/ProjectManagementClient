c3b2d9f6e31ea6b407802f5c45475b535eea5d47
Sobiechh <p.sobieszczyk1@gmail.com>
2021-12-13 03:42:34 +0100

    Add post method add step

--- src/api/utils.tsx
+++ src/api/utils.tsx
@@ -1,6 +1,7 @@
 import { instance } from 'api';
 import SnackbarUtils from '../core/utils/SnackbarUtils';
 import { projectTask } from '../core/types/api/task.request.types';
+import { projectStep } from '../core/types/api/step.request.types';
 
 export const findUsers = async (query: string) => {
   return await instance
@@ -11,6 +12,10 @@ export const findUsers = async (query: string) => {
     .catch((error) => {
       SnackbarUtils.error('Wystąpił problem z wyszukiwaniem');
     });
+};
+
+export const postStep = async (data: projectStep, accessToken: string | null) => {
+  return await instance.post('/Step', data, { headers: { authorization: `Bearer ${accessToken}` } });
 };
 
 export const putTaskApi = async (data: projectTask, accessToken: string) => {
