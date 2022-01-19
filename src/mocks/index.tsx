da9b1faee3d83db61e52a878fe920b380684d3a0
Robert Jaskólski <rjaskolski@i2asolutions.com>
2022-01-19 18:36:03 +0100

    Task Item tests

--- src/mocks/index.tsx
+++ src/mocks/index.tsx
@@ -1,5 +1,6 @@
 import { auth } from 'mocks/handlers/auth';
 import { projects } from './handlers/project';
 import { users } from './handlers/user';
+import { tasks } from './handlers/tasks';
 
-export const handlers = [...auth, ...projects, ...users];
+export const handlers = [...auth, ...projects, ...users, ...tasks];
